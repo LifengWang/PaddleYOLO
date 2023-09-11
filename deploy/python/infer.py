87e212c68cccfbac18dc0dba17c0ed51ee9631f8
LifengWang <lifeng.a.wang@intel.com>
2023-09-11 12:15:56 +0800

    run on cpu

--- deploy/python/infer.py
+++ deploy/python/infer.py
@@ -191,7 +191,7 @@ class Detector(object):
         if run_benchmark:
             for i in range(repeats):
                 self.predictor.run()
-                paddle.device.cuda.synchronize()
+                # paddle.device.cuda.synchronize()
             result = dict(
                 boxes=np_boxes, masks=np_masks, boxes_num=np_boxes_num)
             return result
